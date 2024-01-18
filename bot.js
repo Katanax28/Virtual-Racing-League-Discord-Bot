96a9a0f0eb1de2f6c449d1cab30211fe969cd071
Natan den Hartog <natan2004@live.nl>
2024-01-18 17:40:46 +0100

    setup with test commands

---
+++ bot.js
@@ -0,0 +1,77 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+    ],
+});
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+// Old code before using slash commands
+// client.on("messageCreate", msg => {
+//     if (msg.content === "ping") {
+//         if (msg.member.roles.cache.has("781184561572216832")) {
+//             msg.reply("admin pong");
+//         }else{
+//             msg.reply("pong");
+//         }
+//     }
+//     if (msg.content.startsWith("T1Checkin") && msg.member.roles.cache.has("781184561572216832") && msg.channel.id === "1197557758778679337") {
+//         msg.channel.send("<@401389864077099018>");
+//     }
+// })
+
+
+client.login(token)
---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+    ],
+});
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("messageCreate", msg => {
+    if (msg.content === "ping") {
+        if (msg.member.roles.cache.has("781184561572216832")) {
+            msg.reply("admin pongg");
+        }else{
+            msg.reply("pong");
+        }
+    }
+    if (msg.content.startsWith("T1Checkin") && msg.member.roles.cache.has("781184561572216832") && msg.channel.id === "1197557758778679337") {
+        msg.channel.send("<@401389864077099018>");
+    }
+})
+
+
+client.login('MTE1MDc5Njg5NzYzNjg0MzUyMA.GvUjhB.O5PffbafkkhXZNLq5uW-ncVSuqTVEWsWJtljvU')
