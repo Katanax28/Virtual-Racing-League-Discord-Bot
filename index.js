9151ef6be4b8206d4951f8311be94db7fc4e7a17
jcklerk <justin@klerk.nl>
2024-01-24 17:51:57 +0100

    changed config to .env file

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const token = process.env.TOKEN;
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-    ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
 });
 
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith(".js"));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
+		if ("data" in command && "execute" in command) {
 			client.commands.set(command.data.name, command);
 		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
 		}
 	}
 }
 
-client.once(Events.ClientReady, readyClient => {
+client.once(Events.ClientReady, (readyClient) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, async interaction => {
+client.on(Events.InteractionCreate, async (interaction) => {
 	if (!interaction.isChatInputCommand()) return;
 
 	const command = interaction.client.commands.get(interaction.commandName);
@@ -51,28 +56,37 @@ client.on(Events.InteractionCreate, async interaction => {
 	} catch (error) {
 		console.error(error);
 		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+			await interaction.followUp({
+				content: "There was an error while executing this command!",
+				ephemeral: true,
+			});
 		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			await interaction.reply({
+				content: "There was an error while executing this command!",
+				ephemeral: true,
+			});
 		}
 	}
 });
 
 // Old code before using slash commands
-client.on("messageCreate", msg => {
-    if (msg.content === "woah") {
+client.on("messageCreate", (msg) => {
+	if (msg.content === "woah") {
 		msg.reply("prizm!");
-	}else if(msg.content === "Woah"){
+	} else if (msg.content === "Woah") {
 		msg.reply("Prizm!");
-    }else if(msg.content === "WOAH"){
+	} else if (msg.content === "WOAH") {
 		msg.reply("PRIZM!");
-	}else if(msg.content.includes("woah" || "Woah" || "WOAH")){
+	} else if (msg.content.includes("woah" || "Woah" || "WOAH")) {
 		msg.reply("prizm!");
 	}
-    if (msg.content.startsWith("T1Checkin") && msg.member.roles.cache.has("781184561572216832") && msg.channel.id === "1197557758778679337") {
-        msg.channel.send("<@401389864077099018>");
-    }
-})
+	if (
+		msg.content.startsWith("T1Checkin") &&
+		msg.member.roles.cache.has("781184561572216832") &&
+		msg.channel.id === "1197557758778679337"
+	) {
+		msg.channel.send("<@401389864077099018>");
+	}
+});
 
-
-client.login(token)
+client.login(token);
