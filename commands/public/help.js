f656ccae6b7ac84eda40b3e2d789f2ddcae8b2a9
Natan den Hartog <natan2004@live.nl>
2024-02-02 15:27:14 +0100

    order files better

--- commands/public/help.js
+++ commands/public/help.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 var fs = require('fs');
-var files = fs.readdirSync('./commands/utility/');
+var files = fs.readdirSync('./commands/public/');
 var filesRpl = files.map(file => file.replace(/.js$/, ''));
 
 module.exports = {
