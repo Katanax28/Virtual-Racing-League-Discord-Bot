5a52dad15a95b20f96b06637b092a2ba03d86889
Natan den Hartog <natan2004@live.nl>
2024-04-08 15:58:47 +0200

    Update checkin.js

--- commands/private/checkin.js
+++ commands/private/checkin.js
@@ -2,7 +2,7 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, Permi
 const {Worker} = require("worker_threads");
 require("dotenv").config();
 modChannelId = process.env.DISCORD_MOD_CHANNEL_ID;
-checkinChannelId = process.env.DISCORD_CHECKIN_CHANNEL_ID;
+checkinChannelId = process.env.DISCORD_TEST_CHANNEL_ID;
 
 // function list
 function createButton(customId, label, style) {
@@ -368,6 +368,7 @@ module.exports = {
                     console.error('Something went wrong, your program sucks:', error);
                 }
 
+                await wait(3100);
                 await interaction.deferReply({ephemeral: true});
                 const message = await interaction.message.fetch();
 
