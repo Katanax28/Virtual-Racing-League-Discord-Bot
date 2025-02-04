44b5427daa69f58c30fbfa9414613777357d2785
Natan den Hartog <natan2004@live.nl>
2025-02-04 17:13:52 +0100

    feat: remove swearing flag

--- index.js
+++ index.js
@@ -146,23 +146,23 @@ client.on("messageCreate", async (msg) => {
 		}
 	}
 
-	if(content.includes("fuck") && (!msg.member.roles.cache.some(role => role.name === 'Moderator') && !msg.member.roles.cache.some(role => role.name === 'Admin'))){
-		const now = Date.now();
-
-		if (now < lastUsedTimestamp + cooldownAmount) {
-			return;
-		}
-
-		// Update the last used timestamp
-		lastUsedTimestamp = now;
-
-		await msg.reply(`**Fact:** Language used in Discord server
-		
-**Infringement:** Breach of Article 12.2.1.k of the International Sporting Code
-
-**Decision:** Write formal apology to Admins of the Discord server. 
-		`)
-	}
+// 	if(content.includes("fuck") && (!msg.member.roles.cache.some(role => role.name === 'Moderator') && !msg.member.roles.cache.some(role => role.name === 'Admin'))){
+// 		const now = Date.now();
+//
+// 		if (now < lastUsedTimestamp + cooldownAmount) {
+// 			return;
+// 		}
+//
+// 		// Update the last used timestamp
+// 		lastUsedTimestamp = now;
+//
+// 		await msg.reply(`**Fact:** Language used in Discord server
+//
+// **Infringement:** Breach of Article 12.2.1.k of the International Sporting Code
+//
+// **Decision:** Write formal apology to Admins of the Discord server.
+// 		`)
+// 	}
 });
 
 
