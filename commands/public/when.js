bf9a63488e86da4fd2e8097020e18838bb8153e0
Natan den Hartog <natan2004@live.nl>
2025-02-21 20:15:21 +0100

    fix: update /when information

--- commands/public/when.js
+++ commands/public/when.js
@@ -6,11 +6,12 @@ module.exports = {
 		.setDescription('Lists the time of the races for each tier'),
 	async execute(interaction) {
 		await interaction.reply(`
-Tier 2 races on Saturdays at <t:1662825600:t>
-Tier 1 races on Sundays at <t:1662912000:t>
+We race on Sundays at <t:1662912000:t>
 *These times are displayed in your own timezone, and they are subject to change if they collide with IRL F1 races*
 
 *Also please read <#780985448583528448> before you ask questions to the mods. It saves us a lot of time!*`
 		);
 	},
 };
+
+// Tier 2 races on Saturdays at <t:1662825600:t>
